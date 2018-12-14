0b8fd65647fa4758a7cc59506401b81a25157781
Arun Kumar Vishwakarma <mca.arun9@gmail.com>
2018-12-14 15:49:33 +0530

    Update file_upload_server.php

--- file_upload_server.php
+++ file_upload_server.php
@@ -8,7 +8,7 @@ if (!$fileTmpLoc) { // if file not chosen
     echo "ERROR: Please browse for a file before clicking the upload button.";
     exit();
 }
-if(move_uploaded_file($fileTmpLoc, "test_uploads/$fileName")){
+if(move_uploaded_file($fileTmpLoc, "uploads/$fileName")){
     echo "$fileName upload is complete";
 } else {
     echo "move_uploaded_file function failed";
